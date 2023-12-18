092c5c095cacd52f09f78bf7a343b9ed1d0d9258
sylviazartmann <sylvi_zartmann@rocketmail.com>
2023-12-18 15:04:01 +0100

    videopage, welcome

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,13 @@ import { LoginComponent } from './login/login.component';
 import { VideopageComponent } from './videopage/videopage.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
  export const routes: Routes = [
-//   {path: '', component: StartpageComponent}
+  {path: '', component: WelcomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'videopage', component: VideopageComponent},
-  {path: 'legal-notice', component: LegalNoticeComponent},
-  {path: 'imprint', component: ImprintComponent},
+  {path: 'privacy-policy', component: LegalNoticeComponent},
+  {path: 'legal-notice', component: ImprintComponent},
 ];
